2001475120d5930ebb0db5178bb8236fee30ca3c
mehran13mome <mehran13mome@gmail.com>
2021-05-23 21:14:21 +0430

    initialize Component

---
+++ src/CountDownTimer.js
@@ -0,0 +1,72 @@
+import {useState, useRef, useEffect} from 'react';
+
+const convertMinuteToMilliSeconds = (minutes) => {
+    return minutes * 60 * 1000;
+}
+
+const calculateTimeLeftInMilliSeconds = (timeInMilliSeconds) => {
+    return timeInMilliSeconds.current - new Date().getTime();
+}
+
+function pad(n, z) {
+    z = z || 2;
+    return ('00' + n).slice(-z);
+}
+
+const convertMilliSecondsToTime = (timeInMilliSeconds) => {
+    let seconds = Math.floor((timeInMilliSeconds / 1000) % 60);
+    let minutes = Math.floor((timeInMilliSeconds / (1000 * 60)) % 60);
+    return `${pad(minutes)}:${pad(seconds)}`
+}
+
+const CountDownTimer = ({minutes, onClick}) => {
+
+    const [isStarted, setIsStarted] = useState(true);
+    const initMinutesInMillis = convertMinuteToMilliSeconds(minutes);
+    const [state, setState] = useState(convertMilliSecondsToTime(initMinutesInMillis));
+    const timeToCalculate = useRef(new Date().getTime() + initMinutesInMillis);
+
+    const checkIfTimeLeft = (timeInMilliSeconds) => {
+        if (timeInMilliSeconds > 0){
+            return timeInMilliSeconds;
+        }else{
+            setIsStarted(false);
+            return 0;
+        }
+    }
+
+    const count = () => {
+        console.log("count")
+        setState(convertMilliSecondsToTime(checkIfTimeLeft(calculateTimeLeftInMilliSeconds(timeToCalculate))));
+    }
+
+    const updateCounter = () => {
+        if (isStarted) {
+            count()
+            setInterval(count, 1000);
+        }
+    }
+
+    useEffect(() => {
+        updateCounter()
+    },[])
+
+    const resetComponent = () => {
+        const newInit = convertMinuteToMilliSeconds(minutes);
+        setState(convertMilliSecondsToTime(newInit));
+        timeToCalculate.current = new Date().getTime() + initMinutesInMillis;
+        setIsStarted(true);
+        onClick && onClick();
+    }
+
+    return (
+        <div>
+            {isStarted ?
+                (<h2>`${state} : ${timeToCalculate.current}`</h2>) :
+                (<button onClick={resetComponent}>Resend SmS</button>)
+            }
+        </div>
+    );
+};
+
+export default CountDownTimer;
